572c488d2f726e29eb6a0d3ae36ebd971c04368e
Max Diaz <mdiazn@uni.pe>
2025-08-24 13:22:32 -0500

    deploy in Netlify

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import { GifsApp } from "./GifsApp";
-import { MyCounterApp } from "./counter/components/MyCounterApp";
 
 import "./index.css";
 
