1aecd5527ffd425843595898a5a1e812688322b7
Max Diaz <mdiazn@uni.pe>
2025-08-26 18:46:24 -0500

    mock custom hooks tests

--- src/counter/components/MyCounterApp.test.tsx
+++ src/counter/components/MyCounterApp.test.tsx
@@ -1,41 +1,77 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { MyCounterApp } from "./MyCounterApp";
+
+const handleAddMock = vi.fn();
+const handleSubtractMock = vi.fn();
+
+vi.mock("../hooks/useCounter", () => ({
+  useCounter: () => ({
+    counter: 20,
+    handleAdd: handleAddMock,
+    handleSubtract: handleSubtractMock,
+    handleReset: vi.fn(),
+  }),
+}));
 
 describe("MyCounterApp tests", () => {
   it("should render the component", () => {
     render(<MyCounterApp />);
 
     expect(screen.getByRole("heading", { level: 1 }).innerHTML).toBe(
-      "counter: 0"
+      "counter: 20"
     );
     expect(screen.getByRole("button", { name: "+1" }));
     expect(screen.getByRole("button", { name: "-1" }));
     expect(screen.getByRole("button", { name: "Reset" }));
   });
 
-  it("should increment the count when +1 button is clicked", async () => {
-    const user = userEvent.setup();
+  it("should call handleAdd when +1 button is clicked", async () => {
     render(<MyCounterApp />);
 
-    const count = screen.getByRole("heading", { level: 1 });
-    const addButton = screen.getByRole("button", { name: "+1" });
+    const button = screen.getByRole("button", { name: "+1" });
 
-    await user.click(addButton);
+    await userEvent.click(button);
 
-    expect(count.innerHTML).toBe("counter: 1");
-  });
-
-  it("should decrement the count when -1 button is clicked", async () => {
-    const user = userEvent.setup();
-    render(<MyCounterApp />);
-
-    const count = screen.getByRole("heading", { level: 1 });
-    const button = screen.getByRole("button", { name: "-1" });
-
-    await user.click(button);
-
-    expect(count.innerHTML).toBe("counter: -1");
+    expect(handleAddMock).toHaveBeenCalled();
+    expect(handleSubtractMock).not.toHaveBeenCalled();
   });
 });
+
+// describe("MyCounterApp tests", () => {
+//   it("should render the component", () => {
+//     render(<MyCounterApp />);
+
+//     expect(screen.getByRole("heading", { level: 1 }).innerHTML).toBe(
+//       "counter: 0"
+//     );
+//     expect(screen.getByRole("button", { name: "+1" }));
+//     expect(screen.getByRole("button", { name: "-1" }));
+//     expect(screen.getByRole("button", { name: "Reset" }));
+//   });
+
+//   it("should increment the count when +1 button is clicked", async () => {
+//     const user = userEvent.setup();
+//     render(<MyCounterApp />);
+
+//     const count = screen.getByRole("heading", { level: 1 });
+//     const addButton = screen.getByRole("button", { name: "+1" });
+
+//     await user.click(addButton);
+
+//     expect(count.innerHTML).toBe("counter: 1");
+//   });
+
+//   it("should decrement the count when -1 button is clicked", async () => {
+//     const user = userEvent.setup();
+//     render(<MyCounterApp />);
+
+//     const count = screen.getByRole("heading", { level: 1 });
+//     const button = screen.getByRole("button", { name: "-1" });
+
+//     await user.click(button);
+
+//     expect(count.innerHTML).toBe("counter: -1");
+//   });
+// });
